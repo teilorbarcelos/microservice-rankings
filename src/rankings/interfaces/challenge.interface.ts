e585946334fd068e60f276444d7d6a1c98d31bef
Teilor Souza Barcelos <teilor.barcelos@uds.com.br>
2022-12-04 12:04:37 -0300

    get rankings function done

--- src/rankings/interfaces/challenge.interface.ts
+++ src/rankings/interfaces/challenge.interface.ts
@@ -1,3 +1,4 @@
+import { Document } from 'mongoose';
 import { ChallengeStatus } from './challenge-status.enum';
 
 export interface Challenge extends Document {
--- src/rankings/rankings.service.ts
+++ src/rankings/rankings.service.ts
@@ -7,10 +7,14 @@ import { ClientProxySmartRanking } from 'src/proxyrmq/client-proxy';
 import { Category } from './interfaces/category.interface';
 import { EventName } from './interfaces/event-name.enum';
 import { Play } from './interfaces/play.interface';
-import { RankingResponse } from './interfaces/ranking-response.interface';
+import {
+  PlaysHistory,
+  RankingResponse,
+} from './interfaces/ranking-response.interface';
 import { Ranking } from './interfaces/rankings.schema';
 import * as momentTimezone from 'moment-timezone';
 import { Challenge } from './interfaces/challenge.interface';
+import _ from 'lodash';
 
 @Injectable()
 export class RankingsService {
@@ -96,7 +100,49 @@ export class RankingsService {
         }),
       );
 
-      return;
+      _.remove(rankingRegisters, function (item) {
+        return (
+          challenges.filter((challenge) => challenge._id === item.challenge)
+            .length === 0
+        );
+      });
+
+      this.logger.log(
+        `newRankingRegisters: ${JSON.stringify(rankingRegisters)}`,
+      );
+
+      const result = _(rankingRegisters)
+        .groupBy('player')
+        .map((items, index) => ({
+          player: index,
+          history: _.countBy(items, 'event'),
+          points: _.sumBy(items, 'points'),
+        }))
+        .value();
+
+      const orderedResult = _.orderBy(result, 'points', 'desc');
+
+      this.logger.log(`orderedResult: ${JSON.stringify(orderedResult)}`);
+
+      const rankingResponseList: RankingResponse[] = [];
+
+      orderedResult.map(function (item, index) {
+        const rankingResponse: RankingResponse = {};
+        rankingResponse.player = item.player;
+        rankingResponse.position = index + 1;
+        rankingResponse.score = item.points;
+
+        const playHistory: PlaysHistory = {};
+
+        playHistory.victory = item.history.victory || 0;
+        playHistory.defeats = item.history.defeats || 0;
+
+        rankingResponse.playsHistory = playHistory;
+
+        rankingResponseList.push(rankingResponse);
+      });
+
+      return rankingResponseList;
     } catch (error) {
       this.logger.log(`error: ${JSON.stringify(error.message)}`);
       throw new RpcException(error.message);
